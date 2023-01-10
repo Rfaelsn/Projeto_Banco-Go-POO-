811ae188e648a3b73428966f1596757de6cb7047
Rfaelsn <rafaelnascimentovf6@gmail.com>
2023-01-09 21:51:16 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var titular string = "Rafael"
+	var numeroAgencia int = 589
+	var numeroConta int = 123456
+	var saldo float64 = 120.45
+
+	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
+
+	var titular2 string = "Kérssia"
+	var numeroAgencia2 int = 549
+	var numeroConta2 int = 13456
+	var saldo2 float64 = 125.45
+
+	fmt.Println(titular2, numeroAgencia2, numeroConta2, saldo2)
+}
