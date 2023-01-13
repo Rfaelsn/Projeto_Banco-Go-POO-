7ee7296a05abe7b8c67a4e19547b2005ab835184
Rfaelsn <rafaelnascimentovf6@gmail.com>
2023-01-13 00:56:56 -0300

    criação da função de pagamento de boleto e uso de interface entre as contas poupança e corrente

--- main.go
+++ main.go
@@ -5,9 +5,25 @@ import (
 	"fmt"
 )
 
+func PagarBoleto(conta verificarConta, valorBoleto float64) {
+	conta.Sacar(valorBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
 	contaEmanuel := contas.ContaPoupanca{}
 	contaEmanuel.Depositar(500)
-	contaEmanuel.Sacar(200)
+	PagarBoleto(&contaEmanuel, 100)
+
 	fmt.Println(contaEmanuel.ObterSaldo())
+
+	contaRafael := contas.ContaCorrente{}
+	contaRafael.Depositar(5000)
+	PagarBoleto(&contaRafael, 1000)
+
+	fmt.Println(contaRafael)
+
 }
